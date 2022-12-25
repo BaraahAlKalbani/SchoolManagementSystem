e8876e41bdec11c0363f0ebf8e7dc8d3cbc5b51c
BaraahAlKalbani <baraalkalbani@gmail.com>
2022-12-25 02:17:49 -0800

    updated files

--- src/StarPatternLoop.java
+++ src/StarPatternLoop.java
@@ -55,6 +55,26 @@ public class StarPatternLoop {
 			}
 			System.out.println();
 		}
+		
+		
+		System.out.println("\n");
+
+		
+		for (int i=0; i<5; i++)
+        {
+ 
+            for (int j=5-i; j>1; j--)
+            {
+                System.out.print(" ");
+            }
+  
+            for (int j=0; j<=i; j++ )
+            {
+                System.out.print(" *");
+            }
+  
+            System.out.println();
+        }
 
 
 
